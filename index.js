ecc58e6365af6204513b37257dfa78311b7bfe6a
SANDI MUHAMAD RIZALUL <79769140+PurpleReborn@users.noreply.github.com>
2021-08-24 15:38:49 +0700

    config

--- index.js
+++ index.js
@@ -3,8 +3,25 @@
  */
 import 'react-native-gesture-handler';
 
+import React from 'react';
+import reduxConfig from './src/redux/store';
+import {PersistGate} from 'redux-persist/integration/react';
+import {Provider} from 'react-redux';
+
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const redux = reduxConfig();
+
+const Main = () => {
+  return (
+    <Provider store={redux.store}>
+      <PersistGate persistor={redux.persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  );
+};
+
+AppRegistry.registerComponent(appName, () => Main);
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,27 @@
+import {combineReducers} from 'redux';
+import auth from './auth';
+import user from './user';
+import trx from './trx';
+import chat from './chat';
+
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {persistReducer} from 'redux-persist';
+
+const persistAuth = {
+  key: 'auth',
+  storage: AsyncStorage,
+};
+
+const persistUser = {
+  key: 'user',
+  storage: AsyncStorage,
+};
+
+const rootReducer = combineReducers({
+  auth: persistReducer(persistAuth, auth),
+  // user: persistReducer(persistUser, user),
+  // trx,
+  // chat,
+});
+
+export default rootReducer;
